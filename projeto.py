e42c6ccf8d6205a40cdbaccb3ace1a609f89849e
sportugues2 <sara.portugues@tecnico.ulisboa.com>
2025-01-08 13:29:55 +0000

    1650 no more

--- projeto.py
+++ projeto.py
@@ -14,23 +14,32 @@ fabricas_por_pais = {}
 
 for i in range(fabricas + paises + criancas):
     linha = input()
+
     if i < fabricas:
         ident_fabrica, ident_pais_fabrica, stock_max = map(int, linha.split())
-        lista_fabricas.append((ident_fabrica, ident_pais_fabrica, stock_max))
-        stocks_sum += stock_max
-        if ident_pais_fabrica not in fabricas_por_pais:
-            fabricas_por_pais[ident_pais_fabrica] = []
-        fabricas_por_pais[ident_pais_fabrica].append(ident_fabrica)
+        if stock_max > 0:  # Verifica se o estoque máximo é maior que 0
+            lista_fabricas.append((ident_fabrica, ident_pais_fabrica, stock_max))
+            stocks_sum += stock_max
+            if ident_pais_fabrica not in fabricas_por_pais:
+                fabricas_por_pais[ident_pais_fabrica] = []
+            fabricas_por_pais[ident_pais_fabrica].append(ident_fabrica)
+
     elif i < fabricas + paises:
         ident_pais, limite_exportacoes, min_brinquedos = map(int, linha.split())
-        lista_paises.append((ident_pais, limite_exportacoes, min_brinquedos))
-        min_brinquedos_sum += min_brinquedos
+        if limite_exportacoes > 0:  # Verifica se o limite de exportações é maior que 0
+            lista_paises.append((ident_pais, limite_exportacoes, min_brinquedos))
+            min_brinquedos_sum += min_brinquedos
+            
     else:
         lista = list(map(int, linha.split()))
         id_crianca = lista[0]
         id_pais = lista[1]
         presentes_desejados = lista[2:]
-        lista_criancas.append((id_crianca, id_pais, presentes_desejados))
+        presentes_existentes = []
+        for presentes in presentes_desejados:
+            if lista_fabricas[presentes-1][2] != 0:
+                presentes_existentes.append(presentes)
+        lista_criancas.append((id_crianca, id_pais,presentes_existentes))
 
 if stocks_sum < min_brinquedos_sum:
     print("-1")
@@ -38,33 +47,28 @@ else:
     # Define o problema de maximização
     prob = pulp.LpProblem("Distribuicao_de_Presentes", pulp.LpMaximize)
 
-    # Cria as variáveis de decisão
+    # Cria as variáveis de decisão contínuas
     x = pulp.LpVariable.dicts("x", ((i, j) for i in range(criancas) for j in lista_criancas[i][2]), cat='Binary')
 
     # Função objetivo: Maximizar o número de presentes distribuídos
     prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2])
 
-    # Restrição: Cada criança deve receber exatamente um presente desejado ou nada
-    for i in range(criancas):
-        prob += pulp.lpSum(x[i, j] for j in lista_criancas[i][2]) <= 1
+    # Restrição: Cada criança deve receber no máximo um presente desejado
+    for i in range(len(lista_criancas)):
+        if len(lista_criancas[i][2]) > 1:
+            prob += pulp.lpSum(x[i, j] for j in lista_criancas[i][2]) <= 1
 
-    # Restrição: Cada país deve receber pelo menos o número mínimo de brinquedos
-    for pais in lista_paises:
-        ident_pais = pais[0]
-        min_brinquedos = pais[2]
-        prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if lista_criancas[i][1] == ident_pais) >= min_brinquedos
+    # Combina as restrições de país e estoque de fábrica
+    for ident_pais, limite_exportacoes, min_brinquedos in lista_paises:
+        num_stock = sum(f[2] for f in lista_fabricas if f[1] == ident_pais)
+        if min_brinquedos > 0:
+            prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if lista_criancas[i][1] == ident_pais) >= min_brinquedos
+        if limite_exportacoes < num_stock:
+            prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if j in fabricas_por_pais.get(ident_pais, []) and lista_criancas[i][1] != ident_pais) <= limite_exportacoes
 
     # Restrição: O estoque de cada fábrica não pode ser excedido
-    for fabrica in lista_fabricas:
-        ident_fabrica = fabrica[0]
-        stock_max = fabrica[2]
+    for ident_fabrica, _, stock_max in lista_fabricas:
         prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if j == ident_fabrica) <= stock_max
-
-    # Restrição: Todas as fábricas de um país devem exportar no máximo o limite de exportações
-    for pais in lista_paises:
-        ident_pais = pais[0]
-        limite_exportacoes = pais[1]
-        prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if j in fabricas_por_pais[ident_pais] and lista_criancas[i][1] != ident_pais) <= limite_exportacoes
 
     # Resolve o problema
     prob.solve(pulp.PULP_CBC_CMD(msg=False))
@@ -74,14 +78,3 @@ else:
     else:
         # Imprime os resultados
         print(int(pulp.value(prob.objective)))
-
-
-"""# Restrição: Todas as fábricas de um país devem exportar no máximo o limite de exportações
-for pais in lista_paises:
-    ident_pais = pais[0]
-    limite_exportacoes = pais[1]
-    for fabrica in lista_fabricas:
-        ident_fabrica = fabrica[0]
-        ident_pais_fabrica = fabrica[1]
-        if ident_pais_fabrica == ident_pais:
-            prob += pulp.lpSum(x[i, j] for i in range(criancas) for j in lista_criancas[i][2] if j == ident_fabrica) <= limite_exportacoes"""
